d074cda7f9b744f60da8981d56944164b021e4bc
LA4SHORT <remoteplusva@gmail.com>
2025-10-22 00:36:11 +0100

    Update checkout header, added NotFoundPage

--- ecommerce-project/src/pages/OrdersPage.jsx
+++ ecommerce-project/src/pages/OrdersPage.jsx
@@ -1,42 +1,49 @@
+import { Link } from 'react-router';
 import { Header } from '../components/Header';
+import CartIcon from '../assets/images/icons/cart-icon.png';
+import SearchIcon from '../assets/images/icons/search-icon.png';
+import LogoWhite from '../assets/images/logo-white.png';
+import MobileLogoWhite from '../assets/images/mobile-logo-white.png';
+import BuyAgainIcon from '../assets/images/icons/buy-again.png';
 import './OrdersPage.css';
 
 export function OrdersPage() {
     return (
         <>
             <title>Orders</title>
+            <link rel="icon" type="image/svg+xml" href="orders-favicon.png" />
 
             <Header />
 
             <div className="header">
                 <div className="left-section">
-                    <a href="/" className="header-link">
+                    <Link to="/" className="header-link">
                         <img className="logo"
-                            src="images/logo-white.png" />
+                            src={LogoWhite} />
                         <img className="mobile-logo"
-                            src="images/mobile-logo-white.png" />
-                    </a>
+                            src={MobileLogoWhite} />
+                    </Link>
                 </div>
 
                 <div className="middle-section">
                     <input className="search-bar" type="text" placeholder="Search" />
 
                     <button className="search-button">
-                        <img className="search-icon" src="images/icons/search-icon.png" />
+                        <img className="search-icon" src={SearchIcon} />
                     </button>
                 </div>
 
                 <div className="right-section">
-                    <a className="orders-link header-link" href="/orders">
+                    <Link className="orders-link header-link" to="/orders">
 
                         <span className="orders-text">Orders</span>
-                    </a>
+                    </Link>
 
-                    <a className="cart-link header-link" href="/checkout">
-                        <img className="cart-icon" src="images/icons/cart-icon.png" />
+                    <Link className="cart-link header-link" to="/checkout">
+                        <img className="cart-icon" src={CartIcon} />
                         <div className="cart-quantity">3</div>
                         <div className="cart-text">Cart</div>
-                    </a>
+                    </Link>
                 </div>
             </div>
 
@@ -80,17 +87,17 @@ export function OrdersPage() {
                                     Quantity: 1
                                 </div>
                                 <button className="buy-again-button button-primary">
-                                    <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                                    <img className="buy-again-icon" src={BuyAgainIcon} />
                                     <span className="buy-again-message">Add to Cart</span>
                                 </button>
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
 
                             <div className="product-image-container">
@@ -108,17 +115,17 @@ export function OrdersPage() {
                                     Quantity: 2
                                 </div>
                                 <button className="buy-again-button button-primary">
-                                    <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                                    <img className="buy-again-icon" src={BuyAgainIcon} />
                                     <span className="buy-again-message">Add to Cart</span>
                                 </button>
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
@@ -159,17 +166,17 @@ export function OrdersPage() {
                                     Quantity: 2
                                 </div>
                                 <button className="buy-again-button button-primary">
-                                    <img className="buy-again-icon" src="images/icons/buy-again.png" />
+                                    <img className="buy-again-icon" src={BuyAgainIcon} />
                                     <span className="buy-again-message">Add to Cart</span>
                                 </button>
                             </div>
 
                             <div className="product-actions">
-                                <a href="/tracking">
+                                <Link to="/tracking">
                                     <button className="track-package-button button-secondary">
                                         Track package
                                     </button>
-                                </a>
+                                </Link>
                             </div>
                         </div>
                     </div>
