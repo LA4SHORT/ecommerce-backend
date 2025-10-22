63307effc57f56ff7f452f4236695b09da110fec
LA4SHORT <remoteplusva@gmail.com>
2025-10-23 00:13:31 +0100

    More edits from the lesson

--- ecommerce-project/src/App.jsx
+++ ecommerce-project/src/App.jsx
@@ -1,13 +1,12 @@
 import axios from 'axios';
 import { Routes, Route } from 'react-router';
-import { useState, UseEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { HomePage } from './pages/home/HomePage';
 import { CheckoutPage } from './pages/checkout/CheckoutPage';
 import { OrdersPage } from './pages/orders/OrdersPage';
 import { TrackingPage } from './pages/TrackingPage';
 import { NotFoundPage } from './pages/NotFoundPage';
 import './App.css'
-import { useEffect } from 'react';
 
 function App() {
   const [cart, setCart] = useState([]);
@@ -27,8 +26,8 @@ function App() {
       <Route index element={<HomePage cart={cart} />} />
       <Route path="checkout" element={<CheckoutPage cart={cart} />} />
       <Route path="orders" element={<OrdersPage cart={cart}/>} />
-      <Route path="tracking" element={<TrackingPage />} />
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path="tracking/:orderId/:productId" element={<TrackingPage cart={cart}/>} />
+      <Route path="*" element={<NotFoundPage cart={cart}/>} />
     </Routes>
   )
 }
