3ab1d676646819b33adcfe07f4acf68be619e5f4
LA4SHORT <remoteplusva@gmail.com>
2025-10-23 01:12:31 +0100

    Create addToCart feature

--- ecommerce-project/src/pages/home/HomePage.jsx
+++ ecommerce-project/src/pages/home/HomePage.jsx
@@ -1,11 +1,10 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 import { Header } from '../../components/Header';
-import CheckmarkIcon from '../../assets/images/icons/checkmark.png';
 import { ProductsGrid } from './ProductsGrid';
 import './HomePage.css';
 
-export function HomePage( {cart} ) {
+export function HomePage( { cart, loadCart } ) {
     const [products, setProducts] = useState([])
 
 
@@ -28,7 +27,7 @@ export function HomePage( {cart} ) {
 
 
             <div className="home-page">
-                <ProductsGrid products={products} />
+                <ProductsGrid products={products} loadCart={loadCart} />
             </div>
         </>
     )
